231aad9e104b03b5ccab3693eb09aad5703809d4
Patrick Ian M. Co <patrickianco@outlook.com>
2017-07-11 16:56:36 +0800

    firstRun

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const restify = require('restify');
 const builder = require('botbuilder');
 const config = require('./config');
 const firstRun = require('./dialogs/firstRun');
-// const names = require('./dialogs/guestnames');
-// const guestlist = require('./dialogs/guestlist');
+const names = require('./dialogs/guestnames');
+const guestlist = require('./dialogs/guestlist');
 
 //=======================================================
 // Bot Setup
@@ -32,5 +32,5 @@ server.post('/api/messages', connector.listen());
 //=======================================================
 
 bot.dialog('/', firstRun);
-// bot.dialog('/guestnames', names);
-// bot.dialog('/guestlist', guestlist);
+bot.dialog('/guestlist', guestlist);
+bot.dialog('/guestnames', names);
