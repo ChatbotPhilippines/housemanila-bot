40347c159b88b6fb85502824f28df4e6b70730aa
Patrick Ian M. Co <patrickianco@outlook.com>
2017-07-11 10:33:11 +0800

    first commit

---
+++ constants.js
@@ -0,0 +1,5 @@
+// ./constants.js
+
+module.exports.appID = "205bddac-3dd5-466e-91de-a49e466250ba";
+module.exports.appPassword = "hVRQxv7t6yb4uVoVvwVdwg1";
+module.exports.FB_PAGE_ACCESS_TOKEN = "EAAcMbSiRTlABADYKZAfOzLxQlKBrAsQ17r2amdwZAZBbEeJCHzXWnsLTMdDzqEb2duFTQ3xhZBR9NTYXK8qWRgPn9hvMNuZBCYY4I3B7S1QfJuTEDxbsOmyl59hUrXdkDJ1zEWl7sptV5QN6f8wPZC1hoJA9ZC1qAjhp9v0tBqJlAZDZD";
