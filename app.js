8d883e6278253584fad508d0a1608ad2300494f2
RomeDorado <romedorado@gmail.com>
2017-07-12 13:45:14 +0800

    Asd

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const restify = require('restify');
 const builder = require('botbuilder');
 const config = require('./config');
 const firstRun = require('./dialogs/firstRun');
+const menu = require('./dialogs/menu');
 const guestlist = require('./dialogs/guestlist');
 const bookTable = require('./dialogs/bookTable');
 const names = require('./dialogs/guestnames');
@@ -29,87 +30,6 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 server.post('/api/messages', connector.listen());
 
 
-//=========================================================
-// Bots Middleware
-//=========================================================
-
-// Anytime the major version is incremented any existing conversations will be restarted.
-bot.use(builder.Middleware.firstRun({ version: 1.0, dialogId: '*:/' }));
-bot.use({
-    botbuilder: function (session, next) {
-        if (session.message.text === "GET_STARTED") {
-            session.perUserInConversationData = {};
-            session.userData = {};
-            session.conversationData = {};
-        }
-
-        if (!session.userData.firstRun) {
-            switch (session.message.address.channelId) {
-                case 'facebook':
-                    var params = {
-                        setting_type: "call_to_actions",
-                        thread_state: "new_thread",
-                        call_to_actions: [{
-                            payload: "GET_STARTED"
-                        }]
-                    };
-                    request({
-                        url: `https://graph.facebook.com/v2.6/me/thread_settings?access_token=${PAGE_ACCESS_TOKEN}`,
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        form: params
-                    },
-
-                        function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                session.userData.firstRun = true;
-
-                                var welcomeCard = new builder.HeroCard(session)
-                                    .title('Valkyrie Messenger bot')
-                                    .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
-                                    .images([
-                                        new builder.CardImage(session)
-                                            .url("http://i.imgur.com/fJsZQY6.png")                                      
-                                    ])
-                                    .buttons([
-                                        builder.CardAction.dialogAction(session, "/", "", "Main Menu")
-                                    ]);
-
-                                session.send(new builder.Message(session)
-                                    .addAttachment(welcomeCard));
-                                
-                                next();
-                            } else {
-                                session.userData.firstRun = true;
-                                var welcomeCard = new builder.HeroCard(session)
-                                    .title('Valkyrie Messenger bot')
-                                    .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
-                                    .images([
-                                        new builder.CardImage(session)
-                                            .url("http://i.imgur.com/fJsZQY6.png")                                            
-                                    ])
-                                    .buttons([
-                                        builder.CardAction.imBack(session, "menu", "Main Menu"),
-                                    ]);
-
-                                session.send(new builder.Message(session)
-                                    .addAttachment(welcomeCard));
-                                
-                                next();
-                            }
-                        });
-                    break;
-                
-               
-            }
-
-        } else {
-            next();
-        }
-    }
-});
-
-
 //=======================================================
 // Start of Conversation
 //=======================================================
@@ -117,6 +37,7 @@ bot.use({
 
 
 bot.dialog('/', firstRun);
+bot.dialog('menu', menu);
 bot.dialog('/guestlist', guestlist);
 bot.dialog('/guestnames', names);
 bot.dialog('/bookTable', bookTable);
--- dialogs/firstRun.js
+++ dialogs/firstRun.js
@@ -1,90 +1,19 @@
-'use strict';
-
 var builder = require('botbuilder');
-
 module.exports = [
-        
-    function(session){
-        var selectArray = [
-            "Guest-List",
-            "Book-Table",
-            "Events",
-            "Corporate-Functions"
-        ];
+    function (session) {
+var welcomeCard = new builder.HeroCard(session)
+                                    .title('Valkyrie Messenger bot')
+                                    .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
+                                    .images([
+                                        new builder.CardImage(session)
+                                            .url(`${Constants.BASE_URL}/assets/logo.jpg`)
+                                            .alt('Logo')
+                                    ])
+                                    .buttons([
+                                        builder.CardAction.dialogAction(session, "/menu", " ", "Main Menu"),
+                                    ]);
 
-        var cards = getCards();
-        var reply = new builder.Message(session)
-            .attachmentLayout(builder.AttachmentLayout.carousel)
-            .attachments(cards);
-        session.send("What can I do for you?");
-        builder.Prompts.choice(session, reply, selectArray, { maxRetries:0,promptAfterAction:false});
-
-        function getCards(session){
-            return [
-                new builder.HeroCard(session)
-                .title('Guest List')
-                .images([
-                    builder.CardImage.create(session, 'http://i.imgur.com/fJsZQY6.png')
-                ])
-                .buttons([
-                    builder.CardAction.imBack(session, 'Guest-List', 'Guest List')
-                ]),
-
-                new builder.HeroCard(session)
-                .title('Book a Table')
-                .images([
-                    builder.CardImage.create(session, 'http://i.imgur.com/fJsZQY6.png')
-                ])
-                .buttons([
-                    builder.CardAction.imBack(session, 'Book-Table', 'Book a Table')
-                ]),
-
-                new builder.HeroCard(session)
-                .title('Events')
-                .images([
-                    builder.CardImage.create(session, 'http://i.imgur.com/fJsZQY6.png')
-                ])
-                .buttons([
-                    builder.CardAction.imBack(session, 'Events', 'See Upcoming Events')
-                ]),
-
-                new builder.HeroCard(session)
-                .title('Corporate Functions')
-                .images([
-                    builder.CardImage.create(session, 'http://i.imgur.com/fJsZQY6.png')
-                ])
-                .buttons([
-                    builder.CardAction.imBack(session, 'Corporate-Functions', 'Book an Event')
-                ])
-            ]
-        }
-    },
-    function (session, results){
-        if (results.response){
-            var reply = results.response.entity;
-            switch (reply){
-                case 'Guest-List':
-                    session.replaceDialog('/guestlist');
-                break;
-
-                case 'Book-Table':
-                    session.replaceDialog('/bookTable');
-                break;
-
-                case 'Events':
-                    session.replaceDialog('/events');
-                break;
-
-                case 'Corporate-Functions':
-                    session.replaceDialog('/corporateFunctions');
-                break;
-
-                default:
-                    session.send('May error ka lol.');
-            }
-        }
-        else{
-            session.send('May error ka din lol.');
-        }
+                                session.send(new builder.Message(session)
+                                    .addAttachment(welcomeCard));
     }
 ]
