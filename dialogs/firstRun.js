79bfd714e125bdc0f8a84a15972ffec7385ce4d5
RomeDorado <romedorado@gmail.com>
2017-07-12 13:47:45 +0800

    Asd

--- dialogs/firstRun.js
+++ dialogs/firstRun.js
@@ -6,8 +6,7 @@ var welcomeCard = new builder.HeroCard(session)
                                     .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
                                     .images([
                                         new builder.CardImage(session)
-                                            .url(`${Constants.BASE_URL}/assets/logo.jpg`)
-                                            .alt('Logo')
+                                            .url("http://i.imgur.com/dzy25OF.jpg")                                            
                                     ])
                                     .buttons([
                                         builder.CardAction.dialogAction(session, "/menu", " ", "Main Menu"),
