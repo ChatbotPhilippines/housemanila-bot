cfc11f217cf0ff4e57e3e89840ea172dc0c34de7
RomeDorado <romedorado@gmail.com>
2017-07-12 11:08:20 +0800

    asd

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const firstRun = require('./dialogs/firstRun');
 const guestlist = require('./dialogs/guestlist');
 const bookTable = require('./dialogs/bookTable');
 const names = require('./dialogs/guestnames');
+const PAGE_ACCESS_TOKEN = 'EAAcMbSiRTlABADmflfXlqOWKZBh56eD9ruT9eRwC2oG8UhSndDgUJDZCnsPZAubrfGkRYtUb21qecZCDAlXlElJTQNfUsZBM5kXKBoeUSHJ4esAKhfhqHZCVwZAf3xbqniZCDAbaBJwmHwGWUnSHXhr29batAE5C4jeZBm7s9U9ZBj4gZDZD';
 
 //=======================================================
 // Bot Setup
@@ -28,9 +29,91 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 server.post('/api/messages', connector.listen());
 
 
+//=========================================================
+// Bots Middleware
+//=========================================================
+
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+bot.use({
+    botbuilder: function (session, next) {
+        if (session.message.text === "GET_STARTED") {
+            session.perUserInConversationData = {};
+            session.userData = {};
+            session.conversationData = {};
+        }
+
+        if (!session.userData.firstRun) {
+            switch (session.message.address.channelId) {
+                case 'facebook':
+                    var params = {
+                        setting_type: "call_to_actions",
+                        thread_state: "new_thread",
+                        call_to_actions: [{
+                            payload: "GET_STARTED"
+                        }]
+                    };
+                    request({
+                        url: `https://graph.facebook.com/v2.6/me/thread_settings?access_token=${PAGE_ACCESS_TOKEN}`,
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        form: params
+                    },
+
+                        function (error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                session.userData.firstRun = true;
+
+                                var welcomeCard = new builder.HeroCard(session)
+                                    .title('Valkyrie Messenger bot')
+                                    .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
+                                    .images([
+                                        new builder.CardImage(session)
+                                            .url("http://i.imgur.com/fJsZQY6.png")                                      
+                                    ])
+                                    .buttons([
+                                        builder.CardAction.imBack(session, "menu", "Main Menu"),
+                                    ]);
+
+                                session.send(new builder.Message(session)
+                                    .addAttachment(welcomeCard));
+                                
+                                next();
+                            } else {
+                                session.userData.firstRun = true;
+                                var welcomeCard = new builder.HeroCard(session)
+                                    .title('Valkyrie Messenger bot')
+                                    .subtitle(`Wanna party tonight? Click Main Menu so I can help!`)
+                                    .images([
+                                        new builder.CardImage(session)
+                                            .url("http://i.imgur.com/fJsZQY6.png")                                            
+                                    ])
+                                    .buttons([
+                                        builder.CardAction.imBack(session, "menu", "Main Menu"),
+                                    ]);
+
+                                session.send(new builder.Message(session)
+                                    .addAttachment(welcomeCard));
+                                
+                                next();
+                            }
+                        });
+                    break;
+                
+               
+            }
+
+        } else {
+            next();
+        }
+    }
+});
+
+
 //=======================================================
 // Start of Conversation
 //=======================================================
+
 
 
 bot.dialog('/', firstRun);
