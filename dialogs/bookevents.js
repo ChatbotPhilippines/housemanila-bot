8e56274f37ef03878c05223bcc2729020c5c8e5c
Patrick Ian M. Co <patrickianco@outlook.com>
2017-07-13 17:55:05 +0800

    'bookevents'

--- dialogs/bookevents.js
+++ dialogs/bookevents.js
@@ -3,27 +3,19 @@
 var builder = require('botbuilder');
 
 module.exports = [
-    function(session, args){
-        session.dialogData.events = args || {};
+    function(session){
         builder.Prompts.text(session, 'What is the name of your company?');
     },
     function(session, results){
-        session.dialogData.events.company = results.response;
+        session.userData.company = results.response;
         builder.Prompts.text(session, 'What is your full name?');
     },
     function(session, results){
-        session.dialogData.events.fullname = results.response;
+        session.userData.fullname = results.response;
         builder.Prompts.number(session, 'Please enter your contact number');
     },
     function(session, results){
-        session.dialogData.events.contact = results.response;
+        session.userData.contact = results.response;
         session.replaceDialog('/enteremail', session.userData.emailAdd);
-    },
-    function(session, results){
-        session.dialogData.email = results.response;
-        builder.Prompts.time(session, 'When would you like to have the event? Enter the date and time');
-    },
-    function(session, results){
-        session.endDialog('Thank you for the info! Our events officer will contact you within 24 hours for your inquiry.');
     }
 ]
--- dialogs/enteremail.js
+++ dialogs/enteremail.js
@@ -4,17 +4,26 @@ var builder = require('botbuilder');
 
 module.exports = [
     function(session, args){
-        session.dialogData.emailAdd = args || {};
-        builder.Prompts.text(session, 'Please enter your email address.');
+        if(args && args.reprompt){
+            builder.Prompts.text(session, 'Invalid e-mail address, please enter your e-mail address');
+        }
+        else{
+            builder.Prompts.text(session, 'Please enter your email address.');
+        }
     },
     function(session, results){
         var emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        var emailAdd = session.dialogData.emailAdd = results.response;
+        var emailAdd = results.response;
         if(emailregex.test(emailAdd)){
-            session.endDialogWithResult({response: session.dialogData.emailAdd});
+            session.userData.emailAdd = results.response;
+            builder.Prompts.time(session, 'When would you like to have the event? Enter the date and time.');
         }
         else{
-            session.replaceDialog('/enteremail');
+            session.replaceDialog('/enteremail', {reprompt : true});
         }
+    },
+    function(session, results){
+        session.userData.eventdate = results.response;
+        session.endDialog('Thank you for the info! Our events officer will contact you within 24 hours for your inquiry.');
     }
 ]
