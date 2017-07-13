f0358ed477d8da4413d457348c4ab1926a5348a3
RomeDorado <romedorado@gmail.com>
2017-07-13 12:48:09 +0800

    asd

--- dialogs/tablerates.js
+++ dialogs/tablerates.js
@@ -12,12 +12,11 @@ module.exports = [
         builder.Prompts.choice(session, `Select: `, "Back|Buy Tickets", {listStyle: builder.ListStyle.button});
 
     },
-    function(session,results){
-        var choice = results.response;
-        if (choice == 'Buy Tickets'){
+    function(session,results){        
+        if (results.response == 'Buy Tickets'){
             session.send('For tickets or table reservations, you may contact +639272204244 🙂');            
         }else{
             session.replaceDialog('/tablerates'); 
         }
-    }
+    }//
 ]
