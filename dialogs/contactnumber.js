5b8e0784c42caa35153e890cdca5d79a3094f4aa
RomeDorado <romedorado@gmail.com>
2017-07-13 17:46:46 +0800

    asd

--- dialogs/contactnumber.js
+++ dialogs/contactnumber.js
@@ -12,8 +12,7 @@ module.exports = [
     function(session,results){   
         console.log(results.response);
         session.dialogData.numbers.phone = results.response;
-        if (results.response.entity != null){
-            
+        if (results.response != null){        
 
             builder.Prompts.choice(session, `${session.dialogData.numbers.phone} Is this confirmed?`, "Yes|No", {listStyle: builder.ListStyle.button});
 
@@ -23,9 +22,12 @@ module.exports = [
     function(session,results){   
         console.log(results.response.entity);
         if(results.response.entity == "Yes"){
+
             session.send(`Thank you! A confirmation code will be sent to ${session.dialogData.numbers.phone} within 24 hours to confirm the reservation 🙂`)
+
         }else if (results.response.entity == "No"){
             session.replaceDialog("/contactnumber");
+
         }
 
     }
