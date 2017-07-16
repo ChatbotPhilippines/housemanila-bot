1c0984844f2068371899af246e8d453e92d05250
RomeDorado <romedorado@gmail.com>
2017-07-16 15:17:23 +0800

    phone

--- dialogs/contactnumber.js
+++ dialogs/contactnumber.js
@@ -16,7 +16,7 @@ module.exports = [
     function(session,results){   
         console.log(results.response);
         session.dialogData.numbers.phone = results.response;
-        var phonenumber = session.dialogData.numbers.phone;
+        var phonenumber = session.dialogData.numbers.phone.toString();
         var matched = phonenumber.match(/\d+/g);
         var number = matched ? matched.join('') : '';
         if (number.length == 10 || number.length == 11 || number.length == 9) {                    
