4f4f54b656e38e25490ad98c0b87fc4bf89266f8
RomeDorado <romedorado@gmail.com>
2017-07-20 17:17:27 +0800

    asd

--- dialogs/events.js
+++ dialogs/events.js
@@ -12,18 +12,7 @@ var request = require('request')
         //     "floor-plan",
         //     "buy-tickets"
         // ];
-
-        // var cards = getCards();
-        // var reply = new builder.Message(session)
-        //     .attachmentLayout(builder.AttachmentLayout.carousel)
-        //     .attachments(cards);
-        // session.send("Here are the upcoming events at House Manila");
-        // builder.Prompts.choice(session, reply, { maxRetries:0,promptAfterAction:false});
-
         
-        
-
-        //function getCards(session){
                 var options = { 
                 method: 'GET',
                 url: 'http://7d2fa0f4.ngrok.io/api/events',
@@ -72,37 +61,10 @@ var request = require('request')
     .attachmentLayout(builder.AttachmentLayout.carousel)
     .attachments(elements);
 
-// Show carousel
-session.send("Here are the upcoming events at House Manila");
-session.send(msg);
-        });
-
-
-            // return [
-            //     new builder.HeroCard(session)
-            //     .title('Sample 1')
-            //     .images([
-            //         builder.CardImage.create(session, 'http://i.imgur.com/fJsZQY6.png')
-            //     ])
-            //     .buttons([
-            //         builder.CardAction.imBack(session, 'table-rates', 'Table Rates'),
-            //         builder.CardAction.imBack(session, 'floor-plan', 'Floor Plan'),
-            //         builder.CardAction.imBack(session, 'buy-tickets', 'Buy Tickets')
-
-            //     ])
-
-                // new builder.HeroCard(session)
-                // .title('Sample 2')
-                // .images([
-                //     builder.CardImage.create(session, 'http://i.imgur.com/fJsZQY6.png')
-                // ])
-                // .buttons([
-                //     builder.CardAction.imBack(session, 'table-rates', 'Table Rates'),
-                //     builder.CardAction.imBack(session, 'floor-plan', 'Floor Plan'),
-                //     builder.CardAction.imBack(session, 'buy-tickets', 'Buy Tickets')
-                // ]),                
-  //          ]
-        //}
+        // Show carousel
+        session.send("Here are the upcoming events at House Manila");
+        session.send(msg);
+        });            
     },
     function (session, results){
         if (results.response){
