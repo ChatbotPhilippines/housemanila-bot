85b683d92c539244f71ce7cf0db4850705aade8c
RomeDorado <romedorado@gmail.com>
2017-07-23 19:42:07 +0800

    bday

--- dialogs/bookTable.js
+++ dialogs/bookTable.js
@@ -5,10 +5,13 @@ var consts = require('../helpers/consts');
 module.exports = [
     function(session, args){
         console.log(args);
-
+        if (args != "args"){
         builder.Prompts.choice(session, consts.Prompts.CELEBRATE, "Birthday|Anniversary|Despedida|Bachelor/ette|Others|No Occasion", 
         {listStyle: builder.ListStyle.button});        
-        
+    }else{
+        next();
+    }
+    
     
     },
     // function(session,results){   
@@ -18,7 +21,7 @@ module.exports = [
     //     }
     // }
     
-    function(session, results){  
+    function(session, results, next){  
         if (results.response != null){      
         builder.Prompts.choice(session, consts.Prompts.BIRTHDAY_REQUEST, "Balloons|Party Poppers|Sparklers|Cake|Bottle Parade|Others|None", 
         {listStyle: builder.ListStyle.button});
