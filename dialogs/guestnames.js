7380c79d6130ee700db691abee3d4bd72ad481a2
RomeDorado <romedorado@gmail.com>
2017-07-25 11:26:34 +0800

    body

--- dialogs/guestnames.js
+++ dialogs/guestnames.js
@@ -14,26 +14,52 @@ module.exports = [
     },
     function (session, results){
         if(results.response.entity === 'Yes'){
-            request({
-                uri: 'https://guestlist-app-hm.herokuapp.com/api/guestlist',
-                method: 'POST',
-                headers: {
-                    'access-token': 'eyJhbGciOiJIUzI1NiJ9.c2FtcGxlVG9rZW4.F2vUteLfaWAK9iUKu1PRZnPS2r_HlhzU9NC8zeBN28Q'
-                },
+            // request({
+            //     uri: 'https://guestlist-app-hm.herokuapp.com/api/guestlist',
+            //     method: 'POST',
+            //     headers: {
+            //         'access-token': 'eyJhbGciOiJIUzI1NiJ9.c2FtcGxlVG9rZW4.F2vUteLfaWAK9iUKu1PRZnPS2r_HlhzU9NC8zeBN28Q'
+            //     },
 
-                body: {
-                    "guests": session.userData.guests,
-                    "event_id": session.userData.party,
-                    "app_dtl": {
-                        "app_name": "House Manila",
-                        "app_code": "hm"
-                    }
-                }
-            }, (error, response, body) => {
-                console.log(body);
-                console.log(response);
-                console.log(error);
-            })
+            //     body: {
+            //         "guests": session.userData.guests,
+            //         "event_id": session.userData.party,
+            //         "app_dtl": {
+            //             "app_name": "House Manila",
+            //             "app_code": "hm"
+            //         }
+            //     }
+            // }, (error, response, body) => {
+            //     console.log(body);
+            //     console.log(response);
+            //     console.log(error);
+            // })
+
+            var options = {
+            method: 'POST',
+            url: 'https://events-app-hm.herokuapp.com/api/events',
+            headers: 
+            { 
+                'access-token': 'eyJhbGciOiJIUzI1NiJ9.c2FtcGxlVG9rZW4.F2vUteLfaWAK9iUKu1PRZnPS2r_HlhzU9NC8zeBN28Q',
+                'content-type': 'application/json' 
+            },
+            body: 
+            {   guests: session.userData.guests,
+                event_id: 'session.userData.party',
+                app_dtl: { 
+                    app_name: 'House Manila', 
+                    app_code: 'hm' 
+                } 
+            },
+            json: true 
+            };
+
+            request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+            });
+
             session.send(consts.Messages.PENDING);
             session.endDialog();
         }
