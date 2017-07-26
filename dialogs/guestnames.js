b56367734c596f2e42e26a65210a488ab2a92e22
Patrick Ian M. Co <patrickianco@outlook.com>
2017-07-26 11:10:30 +0800

    as

--- dialogs/guestnames.js
+++ dialogs/guestnames.js
@@ -14,27 +14,6 @@ module.exports = [
     },
     function (session, results){
         if(results.response.entity === 'Yes'){
-            // request({
-            //     uri: 'https://guestlist-app-hm.herokuapp.com/api/guestlist',
-            //     method: 'POST',
-            //     headers: {
-            //         'access-token': 'eyJhbGciOiJIUzI1NiJ9.c2FtcGxlVG9rZW4.F2vUteLfaWAK9iUKu1PRZnPS2r_HlhzU9NC8zeBN28Q'
-            //     },
-
-            //     body: {
-            //         "guests": session.userData.guests,
-            //         "event_id": session.userData.party,
-            //         "app_dtl": {
-            //             "app_name": "House Manila",
-            //             "app_code": "hm"
-            //         }
-            //     }
-            // }, (error, response, body) => {
-            //     console.log(body);
-            //     console.log(response);
-            //     console.log(error);
-            // })
-
             var options = {
             method: 'POST',
             url: 'https://guestlist-app-hm.herokuapp.com/api/guestlist',
