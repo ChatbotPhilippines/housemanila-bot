2c6a638998b615b2ea3899f249935e8536783c44
RomeDorado <romedorado@gmail.com>
2017-07-27 16:54:04 +0800

    asd

--- app.js
+++ app.js
@@ -27,16 +27,6 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   console.log('%s is listening %s', server.name, server.url);
 });
 server.post('/api/messages', connector.listen());
-
-var model = process.env.model ||
-    'https://api.projectoxford.ai/luis/v1/application?id=6c4a0d3e-41ff-4800-9ec7-8fd206ee41e8&subscription-key=692f717f9c3b4f52b852d51c46358315&q=';
-var recognizer = new builder.LuisRecognizer(model)
-var intentDialog = new builder.IntentDialog({
-    recognizers: [recognizer],
-    intentThreshold: 0.5,
-    recognizeMode: builder.RecognizeMode.onBegin
-});
-
 //=======================================================
 // Start of Conversation
 //=======================================================
@@ -54,9 +44,75 @@ bot.dialog('/', [
 bot.dialog('/wit', [
 
 function (session, args, next) {
-  session.send(args);
+  
+  var entity = args || session.message.text;
+  const client = new Wit({accessToken: WIT_TOKEN});
+
+        client.message(entity, {})
+        .then((data) => {
+            var results = data;
+            var entities = results.entities;
+
+            if(Object.keys(entities).length == 0){
+               if(entity !== 'GET_STARTED'){session.send('Sorry, I didn’t quite understand that yet since I’m still a learning bot. Let me store that for future reference. In the mean time, you may contact +639176808888 or type “Menu” if you want to find out the cool things I can do for you!');}
+            }else{
+                var intent = entities.intent[0].value;
+
+                if(('inquiry_type' in entities)){var inquiry_type = entities.inquiry_type[0].value;}
+                if(('emotions' in entities)){var emotion = entities.emotions[0].value;}
+                getWitIntents(intent, inquiry_type,  emotion, session);
+
+            }
+        })
+        .catch(console.error);
+
+  
 }
 ]);
+
+function getWitIntents(intent, inquiry_type, emotion, session){
+
+    switch(intent){
+
+        case 'get_greetings':
+            let random = [ 'Hey, {{fb_first_name}}! Welcome to House Manila! How may I help you?',
+                                'Heeyy!! What can I do for you today?',
+                              'Sup! What can I do for you today?',
+                            'Hi there! 🎉  Type "Menu" to start partying with us!' ];
+            let reply = random[Math.floor(Math.random() * random.length)];
+            session.send(reply);
+        break;
+
+        case 'get_menu':
+            session.beginDialog('/mainMenu');
+        break;
+
+        case 'get_farewell':
+            session.send('Thanks, {{fb_first_name}}! See you at House Manila! Just hit me up whenever you need me :)');
+        break;
+
+        case 'get_compliment':
+            let random = [ 'Aww, thanks! Appreciate it, {{fb_first_name}}!',
+                                '🙈🙈🙈' ]
+            let reply = random[Math.floor(Math.random() * random.length)];
+            session.send(reply);
+        break;
+
+        case 'get_thanks':
+            session.send('No biggie! See you at House Manila!');
+        break;
+
+        case 'get_profanity':
+            let random = ['Hey! Sorry for whatever prompted you to say that. To make your experience better, why not party with us at House Manila? :)',
+                              '💩💩💩',
+                            'Duuuuude!',
+                          'Ey dude, that\'s not cool!'];
+            let reply = random[Math.floor(Math.random() * random.length)];
+            session.send(reply);
+        break;
+
+    }
+}
 
 bot.dialog('/mainMenu', dialogs.menu).triggerAction({matches:/mainMenu/i});
 bot.dialog('/guestlist', dialogs.guestlist).triggerAction({matches:/Guest-List/i});
@@ -72,13 +128,4 @@ bot.dialog('/tablereserve', dialogs.reserve);
 bot.dialog('/seven-fifteen', dialogs.sevenFifteen);
 bot.dialog('/sixteen', dialogs.sixteen);
 bot.dialog('/contactnumber', dialogs.number);
-
-
-// intentDialog.onDefault([
-//     function (session, next) {
-//         session.replaceDialog('/wit');
-//     }
-    
-// ]);
-
 
