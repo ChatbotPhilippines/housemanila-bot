7c6f83e110bef19d4ce7ce7d961d9ab276fdf04d
RomeDorado <romedorado@gmail.com>
2017-08-03 13:41:19 +0800

    aa

--- dialogs/bookTable.js
+++ dialogs/bookTable.js
@@ -72,19 +72,19 @@ module.exports = [
                 // session.send(msg);
                 builder.Prompts.choice(session, msg, selectArray, { maxRetries:0,promptAfterAction:false});
                 var today = new Date();
-                var dd = today.getDate();
-                var mm = today.getMonth()+1; //January is 0!
+                // var dd = today.getDate();
+                // var mm = today.getMonth()+1; //January is 0!
 
-                var yyyy = today.getFullYear();
-                if(dd<10){
-                    dd='0'+dd;
-                } 
-                if(mm<10){
-                    mm='0'+mm;
-                } 
-                var today = dd+'/'+mm+'/'+yyyy;
+                // var yyyy = today.getFullYear();
+                // if(dd<10){
+                //     dd='0'+dd;
+                // } 
+                // if(mm<10){
+                //     mm='0'+mm;
+                // } 
+                // var today = dd+'/'+mm+'/'+yyyy;
                 console.log(today);
-                console.log(Date.getTime());
+                
             });            
     },
     function(session, results, next){
