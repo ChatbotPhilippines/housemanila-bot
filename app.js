e218fdd1bf17c2479ab7a598e87d2fa9f29315a3
RomeDorado <romedorado@gmail.com>
2017-08-09 10:29:36 +0800

    asd

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ bot.dialog('/', [
 bot.dialog('/wit', [
 
 function (session, args, next) {
-  
+  console.log(session.message.From.Name);
   var entity = args || session.message.text;
   const client = new Wit({accessToken: WIT_TOKEN});
 
