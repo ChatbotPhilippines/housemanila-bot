a65384b60eb414d5b7e3e4816f24dfa1bd6aee57
RomeDorado <romedorado@gmail.com>
2017-08-30 13:25:00 +0800

    asd

--- dialogs/tablereserve.js
+++ dialogs/tablereserve.js
@@ -6,20 +6,21 @@ var consts = require('../helpers/consts');
 module.exports = [
     
     function(session){        
-        builder.Prompts.text(session, consts.Prompts.ENTER_EMAIL);
         
+        builder.Prompts.text(session, consts.Prompts.TABLE_RESERVE);  
 
     },
 
         function(session, results){      
-        session.userData.emailAdd = results.response;  
-        builder.Prompts.text(session, consts.Prompts.TABLE_RESERVE);
+        
+        session.userData.name = results.response;
+        builder.Prompts.text(session, consts.Prompts.ENTER_EMAIL);
         
 
     },
     function(session, results){   
         console.log(results.response);
-        session.userData.name = results.response;
+        session.userData.emailAdd = results.response;  
         if (results.response != null){
             builder.Prompts.choice(session, consts.Prompts.GROUP_COUNT, "4-6|7-10|11-15|16+", 
         {listStyle: builder.ListStyle.button});
